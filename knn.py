e2f115cf7ea40faf793f5204483d8bf4c830ab40
vanstrouble <pedrovqz69@gmail.com>
2023-03-19 16:21:40 -0600

    Formated code

--- knn.py
+++ knn.py
@@ -7,16 +7,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris, load_breast_cancer
 
 
-class KNN():
+class KNN:
     def __init__(self, X_train, y_train, k=3) -> None:
         self.k = k
         self.X_train = X_train
         self.y_train = y_train
 
     def predict(self, x):
-        distances = [np.linalg.norm(np.array(x) - np.array(x_train)) for x_train in self.X_train]
+        distances = [
+            np.linalg.norm(np.array(x) - np.array(x_train)) for x_train in self.X_train
+        ]
 
-        k_indices = np.argsort(distances)[:self.k]
+        k_indices = np.argsort(distances)[: self.k]
         k_labels = self.y_train[k_indices]
 
         return Counter(k_labels).most_common()[0][0]
@@ -25,18 +27,28 @@ class KNN():
         return [self.predict(x) for x in X_test]
 
     def _get_distances(self, x, i, j):
-        return [np.linalg.norm(np.array(x) - np.array(x_train)) for x_train in self.X_train[i:j]]
+        return [
+            np.linalg.norm(np.array(x) - np.array(x_train))
+            for x_train in self.X_train[i:j]
+        ]
 
     def _predict(self, x, indices):
         distances = self._get_distances(x, indices[0], indices[1])
-        k_indices = np.argsort(distances)[:self.k]
+        k_indices = np.argsort(distances)[: self.k]
         k_labels = [self.y_train[index] for index in k_indices]
         return Counter(k_labels).most_common()[0][0]
 
     def mp_get_predictions(self, X_test, cpu=cpu_count()):
         with Pool(cpu) as p:
-            indices = [(i * len(self.X_train) // cpu, (i + 1) * len(self.X_train) // cpu) for i in range(cpu)]
-            pool_processes = [p.apply_async(self._predict, args=(x, i)) for x in X_test for i in indices]
+            indices = [
+                (i * len(self.X_train) // cpu, (i + 1) * len(self.X_train) // cpu)
+                for i in range(cpu)
+            ]
+            pool_processes = [
+                p.apply_async(self._predict, args=(x, i))
+                for x in X_test
+                for i in indices
+            ]
             predictions = []
             for i, p_process in enumerate(pool_processes):
                 if i % len(indices) == 0:
@@ -47,6 +59,7 @@ class KNN():
                     predictions.append(results)
         return predictions
 
+
 def evaluate(predictions, y_test):
     accuracy = np.sum(predictions == y_test) / len(y_test)
     # for index, element in enumerate(predictions):
@@ -54,7 +67,7 @@ def evaluate(predictions, y_test):
     return np.around(accuracy, 2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # IrisPlant
     iris = load_iris()
     X, y = iris.data, iris.target
@@ -74,13 +87,13 @@ if __name__ == '__main__':
     normal_cls = KNN(X_train, y_train, k=5)
     normal_pred = normal_cls.get_predictions(X_test)
     end = time.time()
-    print('\nNormal KNN without multiprocessing')
-    print(f'Accuracy: {evaluate(normal_pred, y_test)}, time: {end - start}')
-    
+    print("\nNormal KNN without multiprocessing")
+    print(f"Accuracy: {evaluate(normal_pred, y_test)}, time: {end - start}")
+
     # Multiprocessing KNN
     start = time.time()
     mp_cls = KNN(X_train, y_train, k=3)
     mp_pred = mp_cls.mp_get_predictions(X_test, cpu=2)
     end = time.time()
-    print('\nMultiprocessing KNN')
-    print(f'Accuracy: {evaluate(mp_pred, y_test)}, time: {end - start}')
+    print("\nMultiprocessing KNN")
+    print(f"Accuracy: {evaluate(mp_pred, y_test)}, time: {end - start}")

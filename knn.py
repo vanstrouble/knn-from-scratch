34adc60ced65a93666766c842d1d721942f0d151
vanstrouble <vanstrouble@proton.me>
2024-04-28 15:08:16 -0600

    Add ensure numpy array function to verify every dataset

--- knn.py
+++ knn.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 # from multiprocessing import Pool
 from collections import Counter
@@ -6,33 +7,36 @@ from collections import Counter
 
 class KNN:
     def __init__(self, k=3) -> None:
-        """_summary_
-
-        Args:
-            k (int, optional): K neighbors number. Defaults to 3.
-        """
+        """Init method for KNN"""
         self.k = k
 
     def fit(self, X_train, y_train):
-        """Fit function to lazy lerarning
-
-        Args:
-            X_train (Pandas DataFrame): Training set data
-            y_train (Pandas DataFrame): Labeled training set data
-        """
-        self.X_train = X_train
-        self.y_train = y_train
+        """Fit function to lazy lerarning"""
+        self.X_train = self._ensure_array(X_train)
+        self.y_train = self._ensure_array(y_train)
 
     def predict(self, X_test):
+        """Predict function"""
+        X_test = self._ensure_array(X_test)
         y_pred = [self._compute_distances(x_test) for x_test in X_test]
         return y_pred
 
-    def _compute_distances(self, x_test):
-        distances = [np.linalg.norm(x_test - x_train) for x_train in self.X_train]
+    def _compute_distances(self, x):
+        """Calculate distances between two points"""
+        distances = [np.linalg.norm(x - x_train) for x_train in self.X_train]
         k_indices = np.argsort(distances)[: self.k]
         k_labels = self.y_train[k_indices]
         most_common = Counter(k_labels).most_common()[0][0]
         return most_common
+
+    def _ensure_array(self, data):
+        """Ensure input data is converted to numpy array"""
+        if isinstance(data, pd.DataFrame):
+            return data.values
+        elif isinstance(data, np.ndarray):
+            return data
+        else:
+            raise ValueError("Input data must be a pandas DataFrame or numpy array.")
 
 
 if __name__ == "__main__":
